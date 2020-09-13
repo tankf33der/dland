9d55b2ab377fb9b7ac2442bfcb88ba1dcc38384a
Mike <tankf33der@disroot.org>
2020-09-13 10:33:35 +0300

    .

--- factory.d
+++ factory.d
@@ -1,7 +1,30 @@
 import std.stdio;
 import std.concurrency;
 
+const N = 2;
+
+void worker(int index)
+{
+    uint N = 0;
+
+    receive(
+        (uint s, uint e) {
+            writeln(index, " got new range: ", s, ":", e-1);
+        });
+}
+
 void main ()
 {
+    Tid[N] w;
+    uint N = 3;
+
+    // create
+    for (int i = 0; i < w.length; i++)
+    {
+        w[i] = spawn(&worker, i);
+        w[i].send(N, N+4);
+        N = N+4;
+    }
+
     writeln("start");
 }
