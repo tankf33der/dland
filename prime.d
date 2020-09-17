951bcf03f4a766eeeab7dde7d5d032cc75500a7c
Mike <mpech@tuta.io>
2020-09-17 20:16:25 +0300

    .

--- prime.d
+++ prime.d
@@ -22,6 +22,8 @@ bool isPrime(in BigInt n) @safe
         return false;
     }
 
+    writeln("prime: ", n);
+
 	BigInt d = n - 1;
 	ulong s = 0;
 	while(!(d & 1))
@@ -31,9 +33,9 @@ bool isPrime(in BigInt n) @safe
 	}
 
 	outer:
-	foreach (immutable _; 0..8)
+	foreach (immutable _; 0..16)
 	{
-		ulong a = uniform(2, 1024);
+		ulong a = uniform(2, 16);
 		BigInt b = n / a;
 		BigInt x = powmod(b, d, n);
 		if (x == 1 || x == n - 1)
@@ -50,6 +52,24 @@ bool isPrime(in BigInt n) @safe
 	}
 	return true;
 }
+
+bool isMagna(in BigInt n)
+{
+    BigInt p = 10;
+    while (n >= p)
+    {
+        auto q = n / p;
+        auto r = n % p;
+        writeln(q, ":", r);
+        if(!isPrime(q + r))
+        {
+            return false;
+        }
+        p *= 10;
+    }
+    return true;
+}
+
 
 void main()
 {
@@ -75,4 +95,6 @@ void main()
 	// false
 	writeln(isPrime(cast(BigInt)"4547337172376300111955330758342147474062293202868155909393"));
 	writeln(isPrime(cast(BigInt)"743808006803554439230129854961492699151386107534013432918073439524138264842370630061369715394739134090922937332590384720397133335969549256322620979036686633213903952966175107096769180017646161851573147596390153"));
+
+    writeln(isMagna(BigInt(20261)));
 }
