200057cf38c2f066973b8d37e1a6b2437834e7c9
Mike <mpech@tuta.io>
2020-09-17 16:14:00 +0300

    .

--- prime.d
+++ prime.d
@@ -1,1 +1,14 @@
-#
+import std.stdio;
+import std.file;
+import std.bigint;
+
+void main()
+{
+	auto f = File("tests.dat", "r");
+	BigInt a;
+	foreach(char[] line; f.byLine())
+	{
+		a = cast(BigInt)line;
+	}
+	writeln("start", a);
+}
